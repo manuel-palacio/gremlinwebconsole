468cf1fd972e6410d0c02b91933b96feb9b8f626
Manuel Palacio <emanuel.palacio@gmail.com>
2012-10-18 21:16:37 +0200

    clean up

--- src/main/groovy/org/gremlinconsole/moviegraph/MovieGraphFactory.groovy
+++ src/main/groovy/org/gremlinconsole/moviegraph/MovieGraphFactory.groovy
@@ -2,8 +2,6 @@ package org.gremlinconsole.moviegraph
 
 import com.tinkerpop.blueprints.Graph
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph
-import com.tinkerpop.gremlin.groovy.Gremlin
-import com.tinkerpop.gremlin.Tokens
 
 public class MovieGraphFactory {
 
@@ -13,36 +11,16 @@ public class MovieGraphFactory {
 
     static def createMovieGraph() {
 
-       // Gremlin.load()
-
         if(graph !=  null) return graph
 
         graph = new TinkerGraph()
 
-      //  def genre = [:]
 
-        /*new File("data/u.genre").eachLine {
-            def elements = it.split("\\|")
-            if (elements.size() == 2) {
-                genre.put(elements[1], elements[0])
-            }
-        }*/
 
         new File("data/movies.dat").eachLine {
             def elements = it.split("\\|")
             graph.addVertex(['type': 'Movie', 'movieId': elements[0].toInteger(), 'title': elements[1]])
-            int counter = 0
-            /*elements.each {
-                if (it == "1") {
-                    def genera = genre.get(counter.toString())
-                    if (genera) {
-                        def hits = graph.idx(Tokens.T.v)[[genera: genera]].iterator()
-                        def generaVertex = hits.hasNext() ? hits.next() : graph.addVertex(UUID.randomUUID().toString(),['type': 'Genera', 'genera': genera]);
-                        graph.addEdge(movieVertex, generaVertex, 'hasGenera')
-                    }
-                }
-                counter++
-            }*/
+
         }
 
         new File('data/u.user').eachLine {def line ->
